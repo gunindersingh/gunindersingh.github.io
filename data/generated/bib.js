d5e2027f9779f705839ba5539cdc2d0340ef87dc
gunindersingh <133676249+gunindersingh@users.noreply.github.com>
2023-05-15 15:54:40 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "7845396": {
+        "author": "Wang, Shulong and Hou, Yibin and Gao, Fang and Ji, Xinrong",
+        "booktitle": "2016 IEEE 3rd World Forum on Internet of Things (WF-IoT)",
+        "doi": "10.1109/WF-IoT.2016.7845396",
+        "number": "",
+        "pages": "639-642",
+        "title": "A novel IoT access architecture for vehicle monitoring system",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2016"
+    },
+    "8519843": {
+        "author": "Siddula, Sai Sreekar and Babu, Phaneendra and Jain, P. C.",
+        "booktitle": "2018 3rd International Conference On Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2018.8519843",
+        "number": "",
+        "pages": "1-5",
+        "title": "Water Level Monitoring and Management of Dams using IoT",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2018"
+    },
+    "8519852": {
+        "author": "Mendki, Pankaj",
+        "booktitle": "2018 3rd International Conference On Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2018.8519852",
+        "number": "",
+        "pages": "1-4",
+        "title": "Docker container based analytics at IoT edge Video analytics usecase",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2018"
+    },
+    "8766398": {
+        "author": "Arumugam, Senthamiz Selvi and Badrinath, Ramamurthy and Herranz, Aitor Hernandez and H\u00f6ller, Jan and Azevedo, Carlos R. B. and Xiao, Bin and Tudor, Valentin",
+        "booktitle": "2019 Global IoT Summit (GIoTS)",
+        "doi": "10.1109/GIOTS.2019.8766398",
+        "number": "",
+        "pages": "1-4",
+        "title": "Accelerating Industrial IoT Application Deployment through Reusable AI Components",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8777342": {
+        "author": "Gupta, Alok Kumar and Johari, Rahul",
+        "booktitle": "2019 4th International Conference on Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2019.8777342",
+        "number": "",
+        "pages": "1-5",
+        "title": "IOT based Electrical Device Surveillance and Control System",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8777481": {
+        "author": "Munsadwala, Yashvin and Joshi, Pankti and Patel, Pranav and Rana, Keyur",
+        "booktitle": "2019 4th International Conference on Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2019.8777481",
+        "number": "",
+        "pages": "1-6",
+        "title": "Identification and Visualization of Hazardous Gases Using IoT",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8777607": {
+        "author": "Vishwakarma, Satyendra K. and Upadhyaya, Prashant and Kumari, Babita and Mishra, Arun Kumar",
+        "booktitle": "2019 4th International Conference on Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2019.8777607",
+        "number": "",
+        "pages": "1-4",
+        "title": "Smart Energy Efficient Home Automation System Using IoT",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8777722": {
+        "author": "Singh, Satbir and Singh, Baldev and Ramandeep and Singh, Baljit and Das, Amitava",
+        "booktitle": "2019 4th International Conference on Internet of Things: Smart Innovation and Usages (IoT-SIU)",
+        "doi": "10.1109/IoT-SIU.2019.8777722",
+        "number": "",
+        "pages": "1-6",
+        "title": "Automatic Vehicle Counting for IoT based Smart Traffic Management System for Indian urban Settings",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "8946332": {
+        "author": "G\u00fc\u00e7k\u0131ran, K\u0131van\u00e7 and Bolat, B\u00fclent",
+        "booktitle": "2019 Innovations in Intelligent Systems and Applications Conference (ASYU)",
+        "doi": "10.1109/ASYU48272.2019.8946332",
+        "number": "",
+        "pages": "1-6",
+        "title": "Autonomous Car Racing in Simulation Environment Using Deep Reinforcement Learning",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "9140199": {
+        "author": "Pezol, Nor Syafikah and Adnan, Ramli and Tajjudin, Mazidah",
+        "booktitle": "2020 IEEE International Conference on Automatic Control and Intelligent Systems (I2CACIS)",
+        "doi": "10.1109/I2CACIS49202.2020.9140199",
+        "number": "",
+        "pages": "69-73",
+        "title": "Design of an Internet of Things (Iot) Based Smart Irrigation and Fertilization System Using Fuzzy Logic for Chili Plant",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2020"
+    }
+}});
